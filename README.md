c4b4aefa17f610e602a96f34c25716aad86c3f6a
Lindsey Howard <miss_leading009@yahoo.com>
2022-08-31 18:41:34 -0400

    initial upload

---
+++ README.md
@@ -0,0 +1,53 @@
+# Personal Portfolio
+
+A clean and simple portfolio template built with HTML, CSS and React.js.
+
+Link can be found [here](http://www.lindseyk.dev).
+Contact email is [admin@lindseyk.dev](mailto:admin@lindseyk.dev)
+
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+
+![Material](https://camo.githubusercontent.com/f1ae97a491db2b9e6dcd605d5845e51019c05d2b42bcb22511f4b46f6cf9244e/68747470733a2f2f696d672e736869656c64732e696f2f7374617469632f76313f7374796c653d666f722d7468652d6261646765266d6573736167653d4d6174657269616c2b44657369676e26636f6c6f723d373537353735266c6f676f3d4d6174657269616c2b44657369676e266c6f676f436f6c6f723d464646464646266c6162656c3d)
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+![SCSS](https://camo.githubusercontent.com/7436ecde5696a856dd865d3fc81fa2612054f468e12fdb5d591e7a19a46fc9f7/68747470733a2f2f696d672e736869656c64732e696f2f7374617469632f76313f7374796c653d666f722d7468652d6261646765266d6573736167653d5361737326636f6c6f723d434336363939266c6f676f3d53617373266c6f676f436f6c6f723d464646464646266c6162656c3d)
+
+![my-portfolio](https://user-images.githubusercontent.com/25946305/158276398-86f02299-642f-4f3b-8512-6df4231adf50.jpeg)
+
+---
+
+## Instructions
+
+### Setup
+
+```shell
+git clone https://github.com/codelikeagirl29/react-portfolio.git
+cd react-portfolio
+```
+and then to install and view in browser.
+
+```shell
+npm install
+npm run start
+```
+
+Install and run -
+
+```shell
+yarn
+yarn start
+```
+
+### How to use
+
+- Open `src/info/` and make changes.
+
+
+- Build and deploy -
+
+```shell
+yarn build
+yarn deploy
+```
+---
---
+++ src/info/Info.js
@@ -0,0 +1,128 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+import mock2 from "../img/mock2.png"
+import mock3 from "../img/mock3.png"
+import mock4 from "../img/mock4.png"
+import mock5 from "../img/mock5.png"
+import mock6 from "../img/mock6.jpg"
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+
+export const info = {
+    firstName: "Lindsey",
+    lastName: "Howard",
+    initials: "lh",
+    position: "a Full Stack Developer",
+    selfPortrait: self,
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`,
+    baseColor: colors[0],
+    miniBio: [
+        {
+            emoji: '☕',
+            text: 'fueled by coffee'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in the US'
+        },
+        {
+            emoji: "💼",
+            text: "open to work"
+        },
+        {
+            emoji: "📧",
+            text: "codelikeagirl91@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://facebook.com/codelikeagirl91",
+            icon: 'fa fa-facebook',
+            label: 'facebook'
+        },
+        {
+            link: "https://instagram.com/fiercely.lindseyy",
+            icon: 'fa fa-instagram',
+            label: 'instagram'
+        },
+        {
+            link: "https://github.com/codelikeagirl29",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://linkedin.com/in/lindsey-howard",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+        {
+            link: "https://twitter.com/dev_lindseyk",
+            icon: "fa fa-twitter",
+            label: 'twitter'
+        }
+
+    ],
+    bio: "I am a strategic and creative website developer who specializes in delivering highly impactful websites. I have strong technical skills and experience in web development, software development, and project management. I have a proven track record of consistently meeting deadlines and maximizing results. I am a team player who is able to work effectively in a fast-paced environment.",
+    skills:
+        {
+            proficientWith: ['javascript', 'react', 'git', 'github', 'postgresql', 'nodejs', 'express', 'html/css'],
+            exposedTo: ['nodejs', 'python', 'react native']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'theater',
+            emoji: '🎭'
+        },
+        {
+            label: 'movies',
+            emoji: '🎥'
+        },
+        {
+            label: 'cooking',
+            emoji: '🌶'
+        }
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "City Tours",
+            live: "https://city-tours-seven.vercel.app/",
+            source: "https://github.com/codelikeagirl29/city-tours",
+            image: mock1
+        },
+        {
+            title: "MyReads",
+            live: "https://myreads-react.vercel.app/",
+            source: "https://github.com/CodeLikeAGirl29/nd0191-c1-myreads",
+            image: mock2
+        },
+        {
+            title: "BMW Blog",
+            live: "https://codelikeagirl29.github.io/my-blog/",
+            source: "https://github.com/codelikeagirl29/my-blog",
+            image: mock3
+        },
+        {
+            title: "Students Directory",
+            live: "https://students-pagination-list.netlify.app/",
+            source: "https://github.com/CodeLikeAGirl29/students-pagination",
+            image: mock4
+        },
+        {
+            title: "Unsplash Images",
+            live: "https://xhffdr.csb.app/",
+            source: "https://github.com/CodeLikeAGirl29/unsplash-api",
+            image: mock5
+        },
+        {
+            title: "Node Projects",
+            live: "https://projects-nodejs-express.herokuapp.com",
+            source: "https://github.com/CodeLikeAGirl29/nodejs-and-express",
+            image: mock6
+        }
+    ]
+}
